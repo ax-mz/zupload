561eda4b5b81446e6ccaef4964af4d615b555072
2AM <126387519+ax-mz@users.noreply.github.com>
2025-01-12 19:20:14 +0100

    Add files via upload

---
+++ zupload.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+usage(){
+	cat << EOF
+$(basename $0): Tiny CLI uploader to zupimages.net
+Usage: $(basename $0) <file> [<file>...]
+EOF
+}
+
+! command -v curl >/dev/null && echo "error: missing packet: $_" && exit 1
+[ $# == 0 ] && usage && exit 1
+[ $# -gt 5 ] && echo "error: only 5 images allowed at a time" && exit 1
+
+CMD="curl -s https://www.zupimages.net/up.php"
+
+G=0
+for IMG in "$@"; do [ -f "$IMG" ] && CMD+=" -F files[]=@\"$IMG\"" && ((G++)); done
+
+[ $G == 0 ] && exit 1 || eval "$CMD" | grep 'Lien direct de votre image :'|cut -d'"' -f6|sed 's|//z|//www.z|'
