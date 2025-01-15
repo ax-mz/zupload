8bdba6a36b7132fc60b89dedf8935fe3eb31ca6b
2AM <126387519+ax-mz@users.noreply.github.com>
2025-01-15 19:50:40 +0100

    Update zupload.sh

--- zupload.sh
+++ zupload.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-usage(){
-	cat << EOF
-$(basename $0): Tiny CLI uploader to zupimages.net
-Usage: $(basename $0) <file> [<file>...]
-EOF
-}
+usage(){ echo -e "$(basename $0): Tiny CLI uploader to zupimages.net\nUsage: $(basename $0) <file> [<file>...]"; }
 
 ! command -v curl >/dev/null && echo "error: missing packet: $_" && exit 1
 [ $# == 0 ] && usage && exit 1
@@ -13,7 +8,6 @@ EOF
 
 CMD="curl -s https://www.zupimages.net/up.php"
 
-G=0
-for IMG in "$@"; do [ -f "$IMG" ] && CMD+=" -F files[]=@\"$IMG\"" && ((G++)); done
+G=0; for IMG in "$@"; do [ -f "$IMG" ] && CMD+=" -F files[]=@\"$IMG\"" && ((G++)); done
 
-[ $G == 0 ] && exit 1 || eval "$CMD" | grep 'Lien direct de votre image :'|cut -d'"' -f6|sed 's|//z|//www.z|'
+[ $G == 0 ] && exit 1 || eval "$CMD" |grep 'Lien direct de votre image :'|cut -d'"' -f6|sed 's|//z|//www.z|'
