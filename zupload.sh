72444422457ed4f9dedc86638fabba2bd3b692fa
2AM <126387519+ax-mz@users.noreply.github.com>
2025-01-16 20:34:18 +0100

    no pic number limitation

--- zupload.sh
+++ zupload.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-usage(){ echo -e "$(basename $0): Tiny CLI uploader to zupimages.net\nUsage: $(basename $0) <file> [<file>...]"; }
-
 ! command -v curl >/dev/null && echo "error: missing packet: $_" && exit 1
-[ $# == 0 ] && usage && exit 1
-[ $# -gt 5 ] && echo "error: only 5 images allowed at a time" && exit 1
+[ $# == 0 ] && echo -e "$(basename $0): Tiny CLI uploader for zupimages.net\nUsage: $(basename $0) <file> [<file>...]" && exit 1
 
-CMD="curl -s https://www.zupimages.net/up.php"
-
-G=0; for IMG in "$@"; do [ -f "$IMG" ] && CMD+=" -F files[]=@\"$IMG\"" && ((G++)); done
-
-[ $G == 0 ] && exit 1 || eval "$CMD" |grep 'Lien direct de votre image :'|cut -d'"' -f6|sed 's|//z|//www.z|'
+n=$((($#+5-1)/5))
+for ((i=0;i<$n;i++)); do
+  CMD="curl -s https://www.zupimages.net/up.php"
+  G=0; for IMG in "${@:1:5}"; do [ -f "$IMG" ] && CMD+=" -F files[]=@\"$IMG\"" && ((G++)); done && shift 5
+  [ $G == 0 ] && exit 1 || eval "$CMD" |grep 'Lien direct de votre image :'|cut -d'"' -f6|sed 's|//z|//www.z|'
+done
